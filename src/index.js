f6f757835ef0d7e41c21b761ef6423f749c31c11
benjdg16dev <benjdg16dev@gmail.com>
2023-02-16 17:14:42 -0500

    [feat] config changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+import { Map } from "immutable";
+
+const book = Map({ title: "Harry Potter" });
+
+function publish(book) {
+  return book.set("isPublished", true);
+}
+
+book = publish(book);
+
+console.log(book.toJS());
